9289661978f6a00f027eed12f9ac741b554523b1
mihir-parikh <mihir1408@gmail.com>
2014-01-15 23:12:11 +1100

    First commit of buy_flowers

---
+++ themes/custom/buy_flowers/templates/page.tpl.php
@@ -0,0 +1,12 @@
+<div id = "wrapper">
+	<div id = "navigation">
+		<ul>
+			<li><a href = "#">Home</a></li>
+			<li><a href = "#">About Us</a></li>
+			<li><a href = "#">Delivery</a></li>
+			<li><a href = "#">How to Order</a></li>
+			<li><a href = "#">Contact Us</a></li>
+		</ul>
+	</div>
+	
+</div>
