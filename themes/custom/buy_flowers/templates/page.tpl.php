599fb9c4061b7fd6e7acb318d7dbc49e9c9e6ade
mihir-parikh <mihir1408@gmail.com>
2014-01-19 15:49:49 +1100

    Completion of buy_flowers theme

--- themes/custom/buy_flowers/templates/page.tpl.php
+++ themes/custom/buy_flowers/templates/page.tpl.php
@@ -1,12 +1,22 @@
 <div id = "wrapper">
 	<div id = "navigation">
-		<ul>
-			<li><a href = "#">Home</a></li>
-			<li><a href = "#">About Us</a></li>
-			<li><a href = "#">Delivery</a></li>
-			<li><a href = "#">How to Order</a></li>
-			<li><a href = "#">Contact Us</a></li>
-		</ul>
+		<?php 
+		if ($main_menu){
+			print theme("links__system_main_menu", array("links" => $main_menu)); 
+		}
+		?>
 	</div>
-	
+	<div id = "header"><h1>BuyFlowers.com</h1></div>
+	<div id = "page-wrapper">
+		<div id = "sidebar">
+			<?php print render($page["sidebar_first"]); ?>
+		</div>
+		<div id = "content">
+			<h1><?php print $title; ?></h1>
+			<div class "tabs"><?php print render($tabs); ?></div>
+			<?php print render($page["help"]); ?>
+			<ul class = "action-links"><?php print render($action_links); ?></ul>			
+			<?php print render($page["content"]); ?>
+		</div>
+	</div>
 </div>
