0c27e12153024d46d32ca73c4c3033b5dd0bbc8a
mihir-parikh <mihir1408@gmail.com>
2014-07-15 19:08:18 +1000

    Added css conditionally based on node type

--- themes/edu_theme/template.php
+++ themes/edu_theme/template.php
@@ -4,4 +4,11 @@
  */
 function edu_theme_preprocess_page(&$variables){
     $variables["edu_theme_subheading"] = theme_get_setting("edu_theme_subheading");
+    
+    //Test for article node type
+    if(isset($variables["node"])){
+        if($variables["node"]->type == "article"){
+            drupal_add_css(drupal_get_path("theme", "edu_theme")."/style.css");
+        }
+    }
 }
