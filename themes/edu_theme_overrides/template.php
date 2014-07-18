407a72e44488637906e4476b819cbd4ecf72eab2
mihir-parikh <mihir1408@gmail.com>
2014-07-18 20:04:33 +1000

    Additions to link and image preprocess

--- themes/edu_theme_overrides/template.php
+++ themes/edu_theme_overrides/template.php
@@ -3,9 +3,15 @@
 //Add "THEME_LINK()" to every link
 function edu_theme_overrides_preprocess_link(&$variables){
 //    $variables["text"] .= "THEME_LINK()";
+    if(strpos($variables["text"], "Read more") !== FALSE){
+        $variables["text"] = str_replace("Read more", "Click me!", $variables["text"]);
+    }
 }
 
 function edu_theme_overrides_preprocess_image(&$variables){
-    $variables["path"] = "misc/druplicon.png";
+//    global $user;
+//    if($user->uid == 0){
+//        $variables["path"] = "misc/druplicon.png";
+//    }    
 }
 
